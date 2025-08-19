082c564c9be56309bbd6152c868c30340ff15088
Zakk-Fast <dev@zakkfast.io>
2025-08-19 15:45:30 -0500

    remove trailing slash in url

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ from analyzer import analyze_code
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://code-complexity-analyzer-indol.vercel.app/"], 
+    allow_origins=["http://localhost:3000", "https://code-complexity-analyzer-indol.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
